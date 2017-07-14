2d483f1af565184653bbba5205979e91be4213d8
Butterstroke <Butterstroke@users.noreply.github.com>
2017-07-14 03:41:57 -0400

    Work-in-progress Transfers
    Does not contain working commands. All are requiring to be fixing which is marked on the Trello Board.

--- commands/roll.js
+++ commands/roll.js
@@ -1,15 +1,26 @@
-// Args is how many sides the dice has.
-
 exports.run = function(client, message, args){
-    var x = args;
+  var x = args;
 
-    if (args == '' || args == null) { 
-        return message.channel.send(`I am sorry. Please specify the sided die you wish to roll from.`); 
-    }
+   if (args == '' || args == null) { 
+       return message.channel.send(`I am sorry. Please specify the amount of sides on the die to roll from.`); 
+   }
 
-    min = Math.ceil(1);
-    max = Math.floor(x)
-    y = Math.floor(Math.random() * (max - min + 1)) + min;
+   min = Math.ceil(1);
+   max = Math.floor(x)
+   y = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    message.channel.send(y);
+   message.channel.send(y);
 }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['roll'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'Roll',
+  description: 'Roll command.',
+  usage: 'roll'
+};
