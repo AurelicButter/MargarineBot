b4ee6e9b73ebcefaee4ad74b904ef3cd182416d4
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 17:34:39 -0400

    Update welcome.js
    Fixed all issues given from Codacy.

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,18 +1,18 @@
 const config = require("../settings.json");
 
 exports.run = function(client, message){
-	message.channel.send(`Hello! I am Margarine! I will be a bot user on here so feel free to use me when I am online!`);
-}
+	message.channel.send("Hello! I am Margarine! I will be a bot user on here so feel free to use me when I am online!");
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['welcome'],
+  aliases: ["welcome"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'Welcome',
-  description: 'Send the join message when Margarine joins the server.',
-  usage: 'welcome'
+  name: "Welcome",
+  description: "Send the join message when Margarine joins the server.",
+  usage: "welcome"
 };
