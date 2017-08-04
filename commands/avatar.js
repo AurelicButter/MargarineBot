eb06e4762eda3a92e5557fd747ddcbe7ab1b5948
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 19:46:48 -0400

    Update avatar.js
    Codacy fixes

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,16 +1,16 @@
 exports.run = function(client, message, args){
   message.channel.send(message.author.avatarURL);
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['avatar'],
+  aliases: ["avatar"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'Avatar',
-  description: 'Avatar command.',
-  usage: 'avatar'
+  name: "Avatar",
+  description: "Avatar command.",
+  usage: "avatar"
 };
