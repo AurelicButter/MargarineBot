788fc953793934a7bfaf3612c9b37d4f8272fb2b
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 17:00:17 -0400

    Update warn.js
    Recently added Codacy to check over code. Contains fixes to remove some messages on the dashboard.

--- commands/warn.js
+++ commands/warn.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-    let reason = args.slice(1).join(' ');
+    let reason = args.slice(1).join(" ");
     let user = message.mentions.users.first();
-    let modlog = client.channels.find('name', 'centraloffice')
+    let modlog = client.channels.find("name", "centraloffice")
     
-    if (!modlog) return message.reply('I cannot find the Central Office.'); 
-    if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
-    if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
+    if (!modlog) { return message.reply("I cannot find the Central Office."); }
+    if (reason.length < 1) { return message.reply("You must supply a reason for the warning."); }
+    if (message.mentions.users.size < 1) { return message.reply("You must mention someone to warn them.").catch(console.error); }
 
     const embed = new Discord.RichEmbed()
         .setColor("#ffff00")
@@ -20,12 +20,12 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['w', 'warn'],
+  aliases: ["w", "warn"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'Warn',
-  description: 'Issues a warning to the mentioned user.',
-  usage: 'warn <mention>'
+  name: "Warn",
+  description: "Issues a warning to the mentioned user.",
+  usage: "warn <mention>"
 };
