468ea88f39ed8ba1dbf347fd6e8ac73981965555
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 19:49:47 -0400

    Update version.js
    Codacy fixes.

--- commands/version.js
+++ commands/version.js
@@ -2,17 +2,17 @@ const config = require("../settings.json");
 
 exports.run = function(client, message){
 	message.channel.send(`Margarine is on version: ${config.version}`);
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['v', 'version'],
+  aliases: ["v", "version"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'Version',
-  description: 'Get the current version of Margarine.',
-  usage: 'version'
+  name: "Version",
+  description: "Get the current version of Margarine.",
+  usage: "version"
 };
