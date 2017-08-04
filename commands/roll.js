c54cf5923ae736bd6cf57c146cb4a7913b71021f
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 19:32:45 -0400

    Update roll.js
    More Codacy fixes.

--- commands/roll.js
+++ commands/roll.js
@@ -1,26 +1,26 @@
 exports.run = function(client, message, args){
   var x = args;
 
-   if (args == '' || args == null) { 
-       return message.channel.send(`I am sorry. Please specify the amount of sides on the die to roll from.`); 
-   }
+   if (args === "" || args == null) { 
+       return message.channel.send("I am sorry. Please specify the amount of sides on the die to roll from."); 
+   };
 
    min = Math.ceil(1);
-   max = Math.floor(x)
+   max = Math.floor(x);
    y = Math.floor(Math.random() * (max - min + 1)) + min;
 
    message.channel.send(y);
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['roll'],
+  aliases: ["roll"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'Roll',
-  description: 'Roll command.',
-  usage: 'roll'
+  name: "Roll",
+  description: "Roll command.",
+  usage: "roll"
 };
