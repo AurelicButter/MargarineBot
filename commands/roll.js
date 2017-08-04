b08426713611395856f12f61000beb5a9ae563e5
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 19:38:13 -0400

    Update roll.js
    Too many semi colons.

--- commands/roll.js
+++ commands/roll.js
@@ -3,7 +3,7 @@ exports.run = function(client, message, args){
 
    if (args === "" || args == null) { 
        return message.channel.send("I am sorry. Please specify the amount of sides on the die to roll from."); 
-   };
+   }
 
    min = Math.ceil(1);
    max = Math.floor(x);
