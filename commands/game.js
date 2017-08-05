98efde519c804a9cb638c6778c7e1ec3bc24dd07
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 20:35:23 -0400

    Update game.js
    Codacy fixes

--- commands/game.js
+++ commands/game.js
@@ -2,21 +2,21 @@ const config = require("../settings.json");
 
 exports.run = function(client, message, args){
   args = message.content.split(" ").slice(1).join(" ");
-  if(args == '') { args = null; }
+  if(args === "") { args = null; }
 
-  console.log('Game status changed.');
+  console.log("Game status changed.");
   client.user.setGame(args);
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['game'],
+  aliases: ["game"],
   permLevel: 5
 };
 
 exports.help = {
-  name: 'Game',
-  description: 'Set the game status.',
-  usage: 'game <args>'
+  name: "Game",
+  description: "Set the game status.",
+  usage: "game <args>"
 };
