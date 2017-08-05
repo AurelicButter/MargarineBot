bc255485d2673282cca05140da377287ab478494
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 20:28:03 -0400

    Update ban.js
    More Codacy fixes

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 	let reason = args.slice(1).join(" ");
 	let user = message.mentions.users.first();
-	let modlog = client.channels.find("name", "centraloffice")
+	let modlog = client.channels.find("name", "centraloffice");
 	if (!modlog) { return message.reply("I cannot find the central office!"); }
 	if (reason.length < 1) { return message.reply("You must supply a reason for the ban."); }
 	if (message.mentions.users.size < 1) { return message.reply("You must mention someone to ban them.").catch(console.error); }
@@ -14,9 +14,9 @@ exports.run = (client, message, args) => {
 	const embed = new Discord.RichEmbed()
 		.setColor("#FF0000")
 		.setTimestamp()
-        	.setDescription(`**Action:** Banned\n**User:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-        	.setThumbnail(user.avatarURL);
-    	return client.channels.get(modlog.id).sendEmbed(embed);
+		.setDescription(`**Action:** Banned\n**User:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
+		.setThumbnail(user.avatarURL);
+	return client.channels.get(modlog.id).sendEmbed(embed);
 };
 
 exports.conf = {
