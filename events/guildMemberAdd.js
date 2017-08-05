7b67b88309c2882808095ec9f86103ba443104ab
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-04 20:18:56 -0400

    Update guildMemberAdd.js
    Codacy fixes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
-module.exports = member => {
+module.exports = (member) => {
   let guild = member.guild;
   console.log(`New User: ${member.user.username}`);
   member.guild.defaultChannel.send(`${member.user.username} is buttered up and has joined for the first time!`);
