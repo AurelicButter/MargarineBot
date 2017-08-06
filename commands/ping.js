7c76868872ff612208a6cc4d24f0df4cead5c538
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-06 13:14:04 -0400

    Update ping.js
    Codacy fixes

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,19 @@
 exports.run = function(client, message){
-    message.channel.send('Pinging...')
-        .then(message => {
+    message.channel.send("Pinging...")
+        .then(message) => {
             message.edit(`Pong! (Took: ${message.createdTimestamp - message.createdTimestamp}ms)`);
         });
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['ping'],
+  aliases: ["ping"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'Ping',
-  description: 'Ping/Pong command.',
-  usage: 'ping'
+  name: "Ping",
+  description: "Ping/Pong command.",
+  usage: "ping"
 };
