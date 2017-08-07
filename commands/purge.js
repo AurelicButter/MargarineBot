adf60f4c127e179a6dbd87eedcb0e9a47c1761a3
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-07 18:08:06 -0400

    Update purge.js
    Codacy fixes.

--- commands/purge.js
+++ commands/purge.js
@@ -1,16 +1,16 @@
-const moment = require("moment")
+const moment = require("moment");
 
 exports.run = function(client, message, args) {
     let messagecount = parseInt(args.join(" "));
-	let guild = message.guild.name;
+    let guild = message.guild.name;
 	
     message.channel.fetchMessages({
         limit: messagecount
     }).then(messages => {
-		message.channel.bulkDelete(messages);
-		message.reply(`Purged ${messagecount} messages from the channel, ${message.author.username}.`);
-		console.log(`[${moment().format('YYYY-MM-DD HH:mm')}] ${message.author.username} has purged ${messagecount} messages from ${guild}`);
-	});
+	message.channel.bulkDelete(messages);
+	message.reply(`Purged ${messagecount} messages from the channel, ${message.author.username}.`);
+	console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] ${message.author.username} has purged ${messagecount} messages from ${guild}`);
+    });
 };
 
 exports.conf = {
