d4a3e1184e7bd5afacc40f5157d119527b3c1090
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-07 14:12:05 -0400

    Update purge.js
    Improved purge command with a log and a message in the channel.

--- commands/purge.js
+++ commands/purge.js
@@ -1,19 +1,27 @@
+const moment = require("moment")
+
 exports.run = function(client, message, args) {
-    let messagecount = parseInt(args.join(' '));
+    let messagecount = parseInt(args.join(" "));
+	let guild = message.guild.name;
+	
     message.channel.fetchMessages({
         limit: messagecount
-    }).then(messages => message.channel.bulkDelete(messages));
+    }).then(messages => {
+		message.channel.bulkDelete(messages);
+		message.reply(`Purged ${messagecount} messages from the channel, ${message.author.username}.`);
+		console.log(`[${moment().format('YYYY-MM-DD HH:mm')}] ${message.author.username} has purged ${messagecount} messages from ${guild}`);
+	});
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['purge'],
+  aliases: ["purge"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'Purge',
-  description: 'Purges X amount of messages from a given channel.',
-  usage: 'purge <number>'
+  name: "Purge",
+  description: "Purges X amount of messages from a given channel.",
+  usage: "purge <number>"
 };
