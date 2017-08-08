0ffaf18407b8aa968c0805fc7b20fcd8372993ed
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-08 17:15:49 -0400

    Update poll.js
    Fixed Poll issue with not sending embeds. Also, poll command is fancier with embeds and deleting original command message.

--- commands/poll.js
+++ commands/poll.js
@@ -1,20 +1,31 @@
+const Discord = require("discord.js");
+
 exports.run = function(client, message, args){
-    message.channel.send(`${message.author.username} has started a poll with the question:\n${args}`)
-        .then(message => {
-            message.react('✅');
-            message.react('❌');
-        });
-}
+  args = message.content.split(" ").slice(1).join(" ");
+  if(args.length < 1) { return message.reply("You need to provide a question."); }
+
+  message.delete().catch();
+  const embed = new Discord.RichEmbed()
+     .setColor("#FFFFFF")
+     .setTimestamp()
+     .setDescription(`${message.author.username} has started a poll!`)
+     .addField(`Question: `, `${args}`);
+    return message.channel.send({embed})
+      .then(message => {
+        message.react("✅");
+        message.react("❌");
+       });
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['poll'],
+  aliases: ["poll"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'Poll',
-  description: 'Poll users',
-  usage: 'poll <Topic>'
+  name: "Poll",
+  description: "Poll users",
+  usage: "poll <Topic>"
 };
