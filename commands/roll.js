afeb1c7fbfba19c00c99b3828de9a2a3c043b238
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-10 18:30:31 -0400

    Update roll.js
    Minor updates. Made the response message fancier.

--- commands/roll.js
+++ commands/roll.js
@@ -1,26 +1,24 @@
 exports.run = function(client, message, args){
   var x = args;
 
-   if (args === "" || args == null) { 
-       return message.channel.send("I am sorry. Please specify the amount of sides on the die to roll from."); 
-   }
+   if (args == "" || args == null) { 
+       return message.channel.send(`I am sorry. Please specify the amount of sides on the die to roll from.`); 
+   };
 
-   min = Math.ceil(1);
-   max = Math.floor(x);
-   y = Math.floor(Math.random() * (max - min + 1)) + min;
+   y = Math.floor(Math.random() * (Math.floor(x) - Math.ceil(1) + 1)) + Math.ceil(1);
 
-   message.channel.send(y);
+   message.channel.send(`🎲 You rolled a ${y}! 🎲`);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ["roll"],
-  permLevel: 2
+  permLevel: 0
 };
 
 exports.help = {
   name: "Roll",
-  description: "Roll command.",
-  usage: "roll"
+  description: "Roll a die.",
+  usage: "roll <sides>"
 };
