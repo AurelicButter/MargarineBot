1b5421a50c2abfeb1dbd948e2b1a69f3c2ef5a40
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-14 19:57:21 -0400

    Update help.js
    Codacy fixes.

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,12 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Command List =\n\nUse ${settings.prefix}help <commandname> for details.\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: 'asciidoc'});
+    message.channel.send(`= Command List =\n\nUse ${settings.prefix}help <commandname> for details.\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: 'asciidoc'});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code: 'asciidoc'});
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code: "asciidoc"});
     }
   }
 };
