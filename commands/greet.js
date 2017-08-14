eabb68ab8a68fb3eb5b0fe14ec06b7227db53244
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-14 19:16:32 -0400

    Update greet.js
    Codacy fix

--- commands/greet.js
+++ commands/greet.js
@@ -2,7 +2,7 @@ const moment = require("moment");
 
 exports.run = function(client, message, args){
 	console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Margarine greeted someone.`);
-  	message.delete().catch();
+	message.delete().catch();
 	
 	if (message.mentions.users.size === 0) {
 		return message.channel.send(`Hello ${message.author.username}`);	
