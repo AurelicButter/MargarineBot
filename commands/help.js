49e54301aaf01c96a00fd5a2da6920d2c21f386c
Butterstroke <Butterstroke@users.noreply.github.com>
2017-08-15 02:25:14 -0400

    Update help.js
    Codacy Fix

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Command List =\n\nUse ${settings.prefix}help <commandname> for details.\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: 'asciidoc'});
+    message.channel.send(`= Command List =\n\nUse ${settings.prefix}help <commandname> for details.\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: "asciidoc"});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
