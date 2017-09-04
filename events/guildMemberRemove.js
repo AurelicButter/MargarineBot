9636c8a092722893587a01b28db4857953369c68
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-04 14:31:34 -0400

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = (member => {
-    console.log(`User left: ${member.user.username}`);
-    let guild = member.guild;
-    member.guild.defaultChannel.send(`${member.user.username} has left the server.`);
-};
