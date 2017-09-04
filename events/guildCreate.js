1844517ff8f87395c58478015699921572f57b9e
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-04 14:31:52 -0400

    Delete guildCreate.js

--- events/guildCreate.js
+++
@@ -1,5 +0,0 @@
-module.exports = (guild => {
-    console.log(`I have started working for ${guild.name} at ${new Date()}`);
-    guild.defaultChannel.send("Hello! I am Margarine!");
-    guild.defaultChannel.send("I will be a bot user on here so feel free to use me when I am online!");
-};
