bccf4b3f75ef3e2a5dd4cce07e63607be34661ad
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-04 14:31:14 -0400

    Delete eventLoader.js

--- util/eventLoader.js
+++
@@ -1,9 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`);
-module.exports = (client) => {
-  client.on("ready", () => reqEvent("ready")(client));
-  client.on("message", reqEvent("message"));
-  client.on("guildMemberAdd", reqEvent("guildMemberAdd"));
-  client.on("guildMemberRemove", reqEvent("guildMemberRemove"));
-  client.on("guildBanAdd", reqEvent("guildBanAdd"));
-  client.on("guildBanRemove", reqEvent("guildBanRemove"));
-};
