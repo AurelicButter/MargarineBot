f842b8a2d92c99751a5682b9fe5f876a88283b6e
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-04 14:31:22 -0400

    Delete ready.js

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const moment = require("moment");
-
-module.exports = client => {
-    console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] This is Margarine speaking!`);
-    console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Online and awaiting orders!`);
-};
