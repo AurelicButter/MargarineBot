8bd253c80970fc7af85c67b521dda5d3894f1e93
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-19 08:56:00 -0400

    Update enlargen.js
    Cleaned up the code a bit.

--- commands/general/enlargen.js
+++ commands/general/enlargen.js
@@ -1,19 +1,15 @@
-exports.run = function(client, message, args){
-    if (args.length < 1) {
-        return message.channel.send("You need to give me an emoji!");	
-    } else if (args.length > 1) {
-      return message.channel.send("I can only do one emoji at a time!");
-    } else {
-      //console.log(args);
+exports.run = function(client, message, [args]){
+    if (args.length < 1) { return message.channel.send("You need to give me an emoji!"); }
+    else {
       let emote = client.emojis.find("name", args[0]);
-      if (!emote) { return message.channel.send("I can't find that emoji."); }
+      if (!emote) { return message.channel.send("I can't find that emoji. My searching capabilities are case-sensitive so be sure that the emoji name is **exactly** the way it is spelled."); }
       return message.channel.send(`https://cdn.discordapp.com/emojis/${emote.id}.png`);
     }
 };
 
 exports.conf = {
     enabled: true,
-    runIn: ["text", "dm", "group"],
+    runIn: ["text"],
     aliases: ["see"],
     permLevel: 0,
     botPerms: [],
