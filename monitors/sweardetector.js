df332ec453f1b05ad7950718cc1f268f60a03ef9
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-19 09:19:20 -0400

    Update sweardetector.js
    Another fix to the detector.

--- monitors/sweardetector.js
+++ monitors/sweardetector.js
@@ -1,14 +1,14 @@
 exports.conf = {
-  enabled: true,
-  ignoreBots: false,
-  ignoreSelf: false,
+    enabled: true,
+    ignoreBots: false,
+    ignoreSelf: false,
 };
-
+  
 exports.run = (client, message) => {
-    const swearWords = [/* Insert swearwords in here */];
-    var message = message.content.toLowerCase();
-    if(swearWords.some(word => message.includes(word)) || swearWords.some(word => message.content.includes(word))) {
-        message.delete();
-        message.reply(`You shouldn't have said that!`);
-    }
+  const swearWords = [/* Insert swearwords in here */];
+  var msg = message.content.toLowerCase();
+  if(swearWords.some(word => msg.includes(word)) || swearWords.some(word => message.content.includes(word))) {
+    message.delete();
+    message.reply("You shouldn't have said that!");
+  }
 };
