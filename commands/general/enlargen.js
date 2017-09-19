ebf92306d3c2574d9ccd72a1d4593513f5618742
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-19 09:04:52 -0400

    Update enlargen.js
    Broke the command -> Update Github file -> Run content -> Fails -> Fixes command.
    
    Story of this commit.

--- commands/general/enlargen.js
+++ commands/general/enlargen.js
@@ -1,7 +1,7 @@
-exports.run = function(client, message, [args]){
-    if (args.length < 1) { return message.channel.send("You need to give me an emoji!"); }
+exports.run = function(client, message, [name]){
+    if (name.length < 1) { return message.channel.send("You need to give me an emoji!"); }
     else {
-      let emote = client.emojis.find("name", args[0]);
+      let emote = client.emojis.find("name", name);
       if (!emote) { return message.channel.send("I can't find that emoji. My searching capabilities are case-sensitive so be sure that the emoji name is **exactly** the way it is spelled."); }
       return message.channel.send(`https://cdn.discordapp.com/emojis/${emote.id}.png`);
     }
@@ -19,6 +19,6 @@ exports.conf = {
 exports.help = {
     name: "enlargen",
     description: "Displays an enlargened emoji and url.",
-    usage: "<EmojiName:args>",
+    usage: "<name:str>",
     usageDelim: "",
 };
