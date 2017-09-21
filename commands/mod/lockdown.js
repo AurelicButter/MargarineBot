f7aa4167f01e1b632eea265076d1d6acd8da902f
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-21 15:38:36 -0400

    Update lockdown.js
    Part 1 of reworking commands with functions. Removed a dependency of moment from lock down command.

--- commands/mod/lockdown.js
+++ commands/mod/lockdown.js
@@ -1,20 +1,18 @@
 const ms = require("ms");
-const moment = require("moment");
 
-exports.run = (client, message, [...args]) => {
+exports.run = (client, message, [time, reason]) => {
   if (!client.lockit) client.lockit = [];
-  let time = args.join(" ");
   let validUnlocks = ["release", "unlock"];
   if (!time) { return message.reply("I need a set time to lock the channel down for!"); }
   let checked = message.channel.permissionsFor(message.author.id).has("MUTE_MEMBERS");
   let guild = message.guild;
-  
-  //Lockdown embeds
+
   const Lockembed = new client.methods.Embed()
     .setColor("#FF0000")
     .setTimestamp()
     .setTitle("🔒 LOCKDOWN NOTICE 🔒")
     .setDescription(`This channel has been lockdown by ${message.author.tag} for ${time}`);
+    if (reason != null) { Lockembed.addField("Reason: ", reason); }
 
   const Unlockembed = new client.methods.Embed()
     .setColor("#FF0000")
@@ -33,17 +31,14 @@ exports.run = (client, message, [...args]) => {
   }  
 
   //Action
-  console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Moderator/Admin deployed a lockdown at ${guild.name} for ${time}!`);
   if (validUnlocks.includes(time)) {
-    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: null })
-    .then(() => {
+    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: null }).then(() => {
       message.channel.send({embed: Unlockembed});
       clearTimeout(client.lockit[message.channel.id]);
       delete client.lockit[message.channel.id];
     }).catch(error => { console.log(error); });
   } else {
-    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: false })
-    .then(() => {
+    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: false }).then(() => {
       message.channel.send({embed: Lockembed}).then(() => {
         client.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
@@ -58,16 +53,16 @@ exports.run = (client, message, [...args]) => {
 
 exports.conf = {
     enabled: true,
-    runIn: ["text", "dm", "group"],
-    aliases: ["lockdown", "ld"],
+    runIn: ["text"],
+    aliases: ["ld", "lock"],
     permLevel: 2,
     botPerms: ["MANAGE_ROLES", "EMBED_LINKS", "ADMINISTRATOR"],
     requiredFuncs: [],
 };
   
 exports.help = {
-    name: "lock",
+    name: "lockdown",
     description: "Locks or unlocks the channel.",
-    usage: "<args:str>",
-    usageDelim: " ",
+    usage: "[time:str] [reason:str]",
+    usageDelim: " | ",
 };
