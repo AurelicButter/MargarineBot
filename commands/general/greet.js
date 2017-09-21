db806ec64e43aa0ef2f89169092d56c272204dd4
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-21 14:16:51 -0400

    Update greet.js
    Spacing issues.

--- commands/general/greet.js
+++ commands/general/greet.js
@@ -3,11 +3,11 @@ exports.run = (client, message, [args]) => {
 
     let person = client.funcs.userSearch(client, message, args);
 
-	if (person.id == client.user.id) {
+    if (person.id == client.user.id) {
         return message.channel.send("Why would you try and make me greet myself? I'm not that lonely!");
     }
 
-	return message.channel.send(`Hello ${person.username}! `);
+    return message.channel.send(`Hello ${person.username}! `);
 };
 
 exports.conf = {
