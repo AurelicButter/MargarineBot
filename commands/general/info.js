4483c949e4460665f6ba4b700205df7384d5776f
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-21 23:24:55 -0400

    Update info.js
    Rip. Issue came up and added two lines. At least it is fixed now.

--- commands/general/info.js
+++ commands/general/info.js
@@ -3,15 +3,45 @@ exports.run = function(client, message, [kind, args]){
 
     if (kind == "server" && args != null) { return message.reply("You can't ask information about a server and a user at the same time!"); } 
 
-    if (kind == "user") { user = client.funcs.userSearch(client, message, args); }
-    if (kind == "role") { role = guild.roles.find("name", args); }
+    if (kind == "user") { 
+        user = client.funcs.userSearch(client, message, args); 
 
-    if (user.presence.status === "online") { var Status = "Online"; }
-    if (user.presence.status === "idle") { var Status = "Idle"; }
-    if (user.presence.status === "dnd") { var Status = "Do not Disturb"; }
-    if (user.presence.status === "offline") { var Status = "Offline"; }
+        if (user.presence.status === "online") { var Status = "Online"; }
+        if (user.presence.status === "idle") { var Status = "Idle"; }
+        if (user.presence.status === "dnd") { var Status = "Do not Disturb"; }
+        if (user.presence.status === "offline") { var Status = "Offline"; } 
+        
+        embed = new client.methods.Embed()
+            .setTimestamp()
+            .setAuthor(guild.name, guild.iconURL)
+            .setColor("#4d5fd")
+            .setThumbnail(user.avatarURL)
+            .addField(`User: `, `${user.tag} - ${user.id}`)
+            .addField(`Created: `, user.createdAt.toLocaleString(), true)
+            .addField(`Joined: `, message.guild.members.get(user.id).joinedAt.toLocaleString(), true);
 
-    const Sembed = new client.methods.Embed()
+        if (user.bot === true) { embed.addField("Bot user:", "True", true); } 
+        else { embed.addField("Bot user:", "False", true); }
+
+        if (user.presence.game === null) { embed.addField(`Status: `, Status, true); } 
+        else { embed.addField(`Status: `, `${Status} - ${user.presence.game.name}`, true); }
+    }
+
+    if (kind == "role") { 
+        role = guild.roles.find("name", args); 
+    
+        embed = new client.methods.Embed()
+            .setTimestamp()
+            .setAuthor(guild.name, guild.iconURL)
+            .setColor("#4d5fd")
+            .addField("Role:", `${role.name} - ${role.id}`)
+            .addField("Position:", role.position, true)
+            .addField("Hex Colour:", role.hexColor, true)
+            .addField("Users:", role.members.size, true);
+    }
+
+    if (kind == "server") {
+        embed = new client.methods.Embed()
         .setTimestamp()
         .setAuthor(guild.name, guild.iconURL)
         .setColor("#4d5fd")
@@ -21,34 +51,9 @@ exports.run = function(client, message, [kind, args]){
         .addField(`Owner: `, `${guild.owner.user.tag} - ${guild.owner.id}`)
         .addField(`Members: `, `${guild.memberCount - guild.members.filter(m=>m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true)
         .addField(`Roles: `, guild.roles.size, true);
-
-    const Uembed = new client.methods.Embed()
-        .setTimestamp()
-        .setAuthor(guild.name, guild.iconURL)
-        .setColor("#4d5fd")
-        .setThumbnail(user.avatarURL)
-        .addField(`User: `, `${user.tag} - ${user.id}`)
-        .addField(`Created: `, user.createdAt.toLocaleString(), true)
-        .addField(`Joined: `, message.guild.members.get(user.id).joinedAt.toLocaleString(), true);
-
-        if (user.bot === true) { Uembed.addField("Bot user:", "True", true); } 
-        else { Uembed.addField("Bot user:", "False", true); }
-
-        if (user.presence.game === null) { Uembed.addField(`Status: `, Status, true); } 
-        else { Uembed.addField(`Status: `, `${Status} - ${user.presence.game.name}`, true); }
-
-    const Rembed = new client.methods.Embed()
-        .setTimestamp()
-        .setAuthor(guild.name, guild.iconURL)
-        .setColor("#4d5fd")
-        .addField("Role:", `${role.name} - ${role.id}`)
-        .addField("Position:", role.position, true)
-        .addField("Hex Colour:", role.hexColor, true)
-        .addField("Users:", role.members.size, true);
-
-    if (kind == "server") { return message.channel.send({embed: Sembed}); }
-    if (kind == "user") { return message.channel.send({embed: Uembed}); }
-    if (kind == "role") { return message.channel.send({embed: Rembed}); }
+    }
+    
+    return message.channel.send({embed});
 };
 
 exports.conf = {
