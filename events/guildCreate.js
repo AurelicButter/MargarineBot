c8b665c824d0d0aeef4ffed646a290fc922104b0
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-21 23:57:20 -0400

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,15 +6,11 @@ exports.run = (client, guild) => {
         .setTimestamp()
         .setTitle(`Greetings!`)
         .setDescription(`To the users of ${guild.name},`)
-        .addField("Margarine's Message:", `Hello and thank you for inviting me to ${guild.name}! I am a somewhat helpful and lovely bot created using Komada, a Discord.js framework. To see how I will be able to assist you with, feel free to do m~help. I will send you a list of commands in which you can use. In addition, admins and mods of your server should check the guild configurations and set it as they please. Those commands and explanations are found in the help descriptions.
+        .addField("Margarine's Message:", `Hello and thank you for inviting me to ${guild.name}! I am a somewhat helpful and lovely bot created using Komada, a Discord.js framework. To see how I will be able to assist you with, feel free to do m~help. I will send you a list of commands in which you can use. In addition, the admins and mods of your server should check the guild configurations and set it as they please. Those commands and explanations are found in the help descriptions.
         \nIf you have any improvement ideas, found issues or bugs, or have any complaints, feel free to use my report command and let my creator know how to improve and make me better! However, if I am online, feel free to use me for fun and helpful commands!`)
         .addField("User notice!", `Currently, I have no way of storing permissions for user data based on the Discord's developer TOS. So, by using Margarine, users agree to have their info usable and manipulated for improving and usage of Margarine. Any data collected will be stored securely and safely and Margarine will not ask for any personal information such as a Discord password.`);
 
-    if(guild.channels.exists("name", "general")) { 
-        Channel = guild.channels.find("name", "general"); 
-    } else {
-        Channel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
-    }
+    Channel = client.funcs.defaultChannel(client, Guild);
 
     if (!guild.available) { return; }
 
