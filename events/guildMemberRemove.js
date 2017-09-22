cfb922d96b0bb3859998a9f8bfc7255715f429bd
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-21 23:57:00 -0400

    Update guildMemberRemove.js
    More function slicing fun!

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,11 +4,7 @@ exports.run = (client, guild) => {
     var Guild = guild.guild;
     var member = guild.user;
   
-    if (Guild.channels.exists("name", "general")) { 
-        Channel = Guild.channels.find("name", "general"); 
-    } else {
-        Channel = Guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
-    }
+    Channel = client.funcs.defaultChannel(client, Guild);
 
     console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] User left at ${Guild.name}!`);
     client.channels.get(Channel.id).send(`${member.username} has left the server. Good luck out there!`); 
