c0c3a517e3f51d144afbe8926ccc64353f8dd754
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-23 00:51:43 -0400

    Delete handleMessage.js

--- importation/functions/handleMessage.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client, msg, edited = false) => {
-  if (client.config.ignoreBots && msg.author.bot) return false; // Ignore Bots if True
-  if (client.config.ignoreSelf && msg.author.id === client.user.id) return false; // Ignore Self if true
-  if (client.config.selfbot && msg.author.id !== client.user.id) return false; // Ignore other users if selfbot is true
-  if (!client.config.selfbot && msg.author.id === client.user.id) return false; // Ignore other users if selfbot but config option is false
-  if (!client.config.editableCommands && edited) return false; // Ignore message if owner doesn't allow editableCommands
-  return true;
-};
