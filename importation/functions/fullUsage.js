eea7e5a721ffd7a8439e2d8792859a971427e6c2
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-23 00:51:07 -0400

    Delete fullUsage.js

--- importation/functions/fullUsage.js
+++
@@ -1,1 +0,0 @@
-module.exports = (client, command) => `${client.config.prefix}${command.conf.aliases.length === 0 ? command.help.name : `(${command.help.name}|${command.conf.aliases.join("|")})`}${command.help.usage !== "" ? ` ${command.help.usage.split(" ").join(command.help.usageDelim)}` : ""}`;
