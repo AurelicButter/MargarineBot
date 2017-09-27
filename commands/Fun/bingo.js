0d2443cd9df623f6cfefd9756c86c76b4cdde5c4
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-27 10:36:46 -0400

    Update bingo.js

--- commands/Fun/bingo.js
+++ commands/Fun/bingo.js
@@ -1,11 +1,11 @@
 exports.run = function(client, message){
     let y = Math.floor(Math.random() * (Math.floor(75) - Math.ceil(1) + 1)) + Math.ceil(1);
 
-    if (y < 15){ x = "B"; } 
-    else if (y > 15 && y < 30){ x = "I"; } 
-    else if (y > 30 && y < 45){ x = "N"; } 
-    else if (y > 45 && y < 60){ x = "G"; } 
-    else { x = "O"; }
+    if (y < 15){ var x = "B"; } 
+    else if (y > 15 && y < 30){ var x = "I"; } 
+    else if (y > 30 && y < 45){ var x = "N"; } 
+    else if (y > 45 && y < 60){ var x = "G"; } 
+    else { var x = "O"; }
 
     return message.channel.send(x + y);
 };
@@ -13,7 +13,7 @@ exports.run = function(client, message){
 exports.conf = {
     enabled: true,
     runIn: ["text"],
-    aliases: [""],
+    aliases: [],
     permLevel: 0,
     botPerms: [],
     requiredFuncs: [],
