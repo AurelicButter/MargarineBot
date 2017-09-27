966b4947340b35a5966ed4aa4d651ad507934641
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-27 10:28:32 -0400

    Delete botPermissions.js

--- importation/functions/botPermissions.js
+++
@@ -1,3 +0,0 @@
-const { Permissions } = require("discord.js");
-
-module.exports = client => Permissions.resolve([...new Set(client.commands.reduce((a, b) => a.concat(b.conf.botPerms), ["READ_MESSAGES", "SEND_MESSAGES"]))]);
