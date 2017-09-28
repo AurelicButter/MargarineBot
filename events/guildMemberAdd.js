c4b2a7d29db0fb48075ca71a20cb8479d1fd7c01
Butterstroke <konirosen@protonmail.ch>
2017-09-28 09:31:14 -0400

    Update guildMemberAdd.js
    Codacy fix.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,8 +4,8 @@ exports.run = (client, guild) => {
   var Guild = guild.guild;
   var member = guild.user;
 
-  Channel = client.funcs.defaultChannel(client, Guild);
+  var Channel = client.funcs.defaultChannel(client, Guild);
 
   console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] New User at ${Guild.name}!`);
-  client.channels.get(Channel.id).send(`${member.username} is buttered up and has joined for the first time!`); 
+  client.channels.get(Channel.id).send(`${member.username} is buttered up and has joined the server!`); 
 };
