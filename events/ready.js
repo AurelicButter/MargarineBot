ce11ab2ff11ef40a6cda57b08e6d34d5aee7921b
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-27 21:35:35 -0400

    Update ready.js
    Whoo! Presence fixes and no more time tick improvements! o/

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,7 @@
 const moment = require("moment");
 
-exports.run = async client => { 
-    //await <Time ticking?>
-    await client.user.setPresence({ game:  { name:` m~help  | Playing around with Butter on ${client.guilds.size} servers`, type: 0 } });
-    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] This is Margarine speaking!`);
-    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Online and awaiting orders!`);
-};
+exports.run = async client => {
+    await client.user.setPresence({ activity:  { name:` m~help  | Playing around with Butter on ${client.guilds.size} servers`, type: 0 } });
+    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] This is Margarine speaking! Online and awaiting orders!`);
+    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Current status: Serving ${client.guilds.size} guilds and ${client.users.size} people.`)
+}
