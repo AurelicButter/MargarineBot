7bce6ac4d5b93905e7b2c6b0f1408180d0af3734
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-27 22:40:35 -0400

    Update ready.js
    Codacy fixes.

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const moment = require("moment");
 exports.run = async client => {
     await client.user.setPresence({ activity:  { name:` m~help  | Playing around with Butter on ${client.guilds.size} servers`, type: 0 } });
     await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] This is Margarine speaking! Online and awaiting orders!`);
-    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Current status: Serving ${client.guilds.size} guilds and ${client.users.size} people.`)
-}
+    await console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] Current status: Serving ${client.guilds.size} guilds and ${client.users.size} people.`);
+};
