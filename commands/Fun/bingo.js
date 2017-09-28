590cccbec0317e6a19c49fe370b636dac20cdee2
Butterstroke <Butterstroke@users.noreply.github.com>
2017-09-27 22:39:49 -0400

    Update bingo.js
    Possible Codacy fix.

--- commands/Fun/bingo.js
+++ commands/Fun/bingo.js
@@ -1,11 +1,12 @@
 exports.run = function(client, message){
     let y = Math.floor(Math.random() * (Math.floor(75) - Math.ceil(1) + 1)) + Math.ceil(1);
+    let x = null;
 
-    if (y < 15){ var x = "B"; } 
-    else if (y > 15 && y < 30){ var x = "I"; } 
-    else if (y > 30 && y < 45){ var x = "N"; } 
-    else if (y > 45 && y < 60){ var x = "G"; } 
-    else { var x = "O"; }
+    if (y < 15){ x = "B"; } 
+    else if (y > 15 && y < 30){ x = "I"; } 
+    else if (y > 30 && y < 45){ x = "N"; } 
+    else if (y > 45 && y < 60){ x = "G"; } 
+    else { x = "O"; }
 
     return message.channel.send(x + y);
 };
