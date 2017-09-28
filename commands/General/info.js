46685a7c858a9e18d2f12d619f608f164e2f3013
Butterstroke <konirosen@protonmail.ch>
2017-09-28 14:31:20 -0400

    Update info.js
    Codacy fixes and fixed from upgraded Discord.js.

--- commands/General/info.js
+++ commands/General/info.js
@@ -1,31 +1,32 @@
 exports.run = (client, message, [kind, User]) => {
     let guild = message.guild;
+    let Status = null;
 
     if (kind === "server" && User != null) { return message.reply("You can't ask information about a server and a user at the same time!"); } 
 
     const embed = new client.methods.Embed()
         .setTimestamp()
-        .setAuthor(guild.name, guild.iconURL)
+        .setAuthor(guild.name, guild.iconURL())
         .setColor("#4d5fd");
 
     if (kind === "user") { 
-        var user = client.funcs.userSearch(client, message, User); 
+        let user = client.funcs.userSearch(client, message, User); 
 
-        if (user.presence.status === "online") { var Status = "Online"; }
-        if (user.presence.status === "idle") { var Status = "Idle"; }
-        if (user.presence.status === "dnd") { var Status = "Do not Disturb"; }
-        if (user.presence.status === "offline") { var Status = "Offline"; } 
+        if (user.presence.status === "online") { Status = "Online"; }
+        if (user.presence.status === "idle") { Status = "Idle"; }
+        if (user.presence.status === "dnd") { Status = "Do not Disturb"; }
+        if (user.presence.status === "offline") { Status = "Offline"; } 
         
-        embed.setThumbnail(user.avatarURL)
-        .addField(`User: `, `${user.tag} - ${user.id}`)
-        .addField(`Created: `, user.createdAt.toLocaleString(), true)
-        .addField(`Joined: `, message.guild.members.get(user.id).joinedAt.toLocaleString(), true);
+        embed.setThumbnail(user.avatarURL())
+        .addField("User:", `${user.tag} - ${user.id}`)
+        .addField("Created:", user.createdAt.toLocaleString(), true)
+        .addField("Joined:", message.guild.members.get(user.id).joinedAt.toLocaleString(), true);
 
         if (user.bot === true) { embed.addField("Bot user:", "True", true); } 
         else { embed.addField("Bot user:", "False", true); }
 
-        if (user.presence.game === null) { embed.addField(`Status: `, Status, true); } 
-        else { embed.addField(`Status: `, `${Status} - ${user.presence.game.name}`, true); }
+        if (user.presence.activity === null) { embed.addField("Status:", Status, true); } 
+        else { embed.addField("Status:", `${Status} - ${user.presence.activity.name}`, true); }
     }
 
     if (kind === "role") { 
@@ -38,12 +39,12 @@ exports.run = (client, message, [kind, User]) => {
     }
 
     if (kind === "server") {
-        embed.setThumbnail(guild.iconURL)
-        .addField(`Region: `, guild.region, true)
-        .addField(`Created: `, guild.createdAt.toLocaleString(), true)
-        .addField(`Owner: `, `${guild.owner.user.tag} - ${guild.owner.id}`)
-        .addField(`Members: `, `${guild.memberCount - guild.members.filter(m=>m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true)
-        .addField(`Roles: `, guild.roles.size, true);
+        embed.setThumbnail(guild.iconURL())
+        .addField("Region:", guild.region, true)
+        .addField("Created:", guild.createdAt.toLocaleString(), true)
+        .addField("Owner:", `${guild.owner.user.tag} - ${guild.owner.id}`)
+        .addField("Members:", `${guild.memberCount - guild.members.filter(m => m.user.bot).size} (${guild.members.filter(m=>m.user.bot).size} bots)`, true)
+        .addField("Roles:", guild.roles.size, true);
     }
 
     return message.channel.send({embed});
