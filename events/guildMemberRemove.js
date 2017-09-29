b0e254f7ff859031691aaf89303fcaf68d0bff27
Butterstroke <konirosen@protonmail.ch>
2017-09-28 23:57:41 -0400

    Update guildMemberRemove.js
    Codacy fixes.

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,7 +4,7 @@ exports.run = (client, guild) => {
     var Guild = guild.guild;
     var member = guild.user;
   
-    Channel = client.funcs.defaultChannel(client, Guild);
+    let Channel = client.funcs.defaultChannel(client, Guild);
 
     console.log(`[${moment().format("YYYY-MM-DD HH:mm")}] User left at ${Guild.name}!`);
     client.channels.get(Channel.id).send(`${member.username} has left the server. Good luck out there!`); 
