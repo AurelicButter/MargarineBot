09171dc151edbd6a74157d22365e4d0ddd67a952
Butterstroke <konirosen@protonmail.ch>
2017-10-23 14:27:31 -0400

    Update rep.js

--- commands/Economy/rep.js
+++ commands/Economy/rep.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, [member]) => {
-    const sql = require("sqlite");
-    sql.open("./bwd/data/score.sqlite");
+    const sqlite3 = require("sqlite3").verbose();
+    let db = new sqlite3.Database("./bwd/data/score.sqlite");
 
     var user = client.funcs.userSearch(client, message, member);
     
@@ -8,22 +8,21 @@ exports.run = async (client, message, [member]) => {
     if (user.bot === true) { return message.reply("You can't give reputation to a bot user!"); }
     if (user.id === message.author.id) { return message.reply("You can't give reputation to yourself! That's like saying hire me for your nuclear plant because I'm a high school student!"); }
 
-    sql.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`).then(row => {
+    db.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`, [], (err, row) => {
+        if (err) { console.log(err); }
         if (!row) { return message.reply("You have not redeemed your first daily yet!"); }
         if ((parseInt(row.repDaily) + 86400000) > Date.now()) { return message.reply("You've already have given someone else rep today!"); }
         else { 
-            sql.get(`SELECT * FROM scores WHERE userId = "${user.id}"`).then(row => {
+            db.get(`SELECT * FROM scores WHERE userId = "${user.id}"`, [], (err, row) => {
+                if (err) { console.log(err); }
                 if (!row) { return message.channel.send(`That user has not gotten their first daily to start off with so you can not give them any rep at the moment. :cry:`); } 
                 else {
-                    sql.run(`UPDATE scores SET rep = ${row.rep + 1} WHERE userId = ${user.id}`);
-                    sql.run(`UPDATE scores SET repDaily = ${Date.now()} WHERE userId = ${message.author.id}`); 
+                    db.run(`UPDATE scores SET rep = ${row.rep + 1} WHERE userId = ${user.id}`);
+                    db.run(`UPDATE scores SET repDaily = ${Date.now()} WHERE userId = ${message.author.id}`); 
                     return message.channel.send(`You have given <@${user.id}> a reputation point!`);
                 }
             });
         }
-    }).catch(error => { 
-        console.log(error);
-        return message.reply("Error in command. Please try again later!");
     });
 };
 
