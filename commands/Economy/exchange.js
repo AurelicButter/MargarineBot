333c18ccb5e209693869731e4db6dfb9990a240b
Butterstroke <konirosen@protonmail.ch>
2017-10-23 14:19:02 -0400

    Update exchange.js

--- commands/Economy/exchange.js
+++ commands/Economy/exchange.js
@@ -1,29 +1,28 @@
 exports.run = async (client, message, [user, credit]) => {
-    const sql = require("sqlite");
-    sql.open("./bwd/data/score.sqlite");
+    const sqlite3 = require("sqlite3").verbose();
+    let db = new sqlite3.Database("./bwd/data/score.sqlite");
 
     var user = client.funcs.userSearch(client, message, user);
     
     if (user.username === null || user.username === undefined) { return; }
     if (user.bot === true) { return message.reply("You can't give your credits to a bot user!"); }
 
-    sql.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`).then(row => {
+    db.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`, [], (err, row) => {
+        if (err) { throw err; }
         if (!row) { return message.reply("You haven't signed up and received your credits yet! D: Use `m~daily` (Using default prefix) to earn your first amount of credits."); } 
         if (row.credits < credit) { return message.reply("You don't have that many credits, baka!"); }
         if (!credit || credit < 1) { return message.reply("You can't just give an invisable amount of credits to someone!"); }
         else { 
-            sql.get(`SELECT * FROM scores WHERE userId = "${user.id}"`).then(row => {
+            db.get(`SELECT * FROM scores WHERE userId = "${user.id}"`, [], (err, row) => {
+                if (err) { throw err; }
                 if (!row) { return message.reply("That user has not gotten their first daily yet!"); }
-                else { sql.run(`UPDATE scores SET credits = ${parseInt(row.credits) + parseInt(credit)} WHERE userId = ${user.id}`); }
+                else { db.run(`UPDATE scores SET credits = ${parseInt(row.credits) + parseInt(credit)} WHERE userId = ${user.id}`); }
             });
-            sql.run(`UPDATE scores SET credits = ${parseInt(row.credits) - parseInt(credit)} WHERE userId = ${message.author.id}`); 
+            db.run(`UPDATE scores SET credits = ${parseInt(row.credits) - parseInt(credit)} WHERE userId = ${message.author.id}`); 
+            message.reply(`You have given ${credit} credits to ${user.username}`);
         }
-    }).catch(error => { 
-        console.log(error);
-        return message.reply("Error in command. Please try again later.");
     });
-
-    message.reply(`You have given ${credit} credits to ${user.username}`);
+    db.close();
 };
 
 exports.conf = {
