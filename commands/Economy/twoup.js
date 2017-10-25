16fc533c6f06e104bee7682073c521c8f02a7aae
Butterstroke <konirosen@protonmail.ch>
2017-10-24 21:46:57 -0400

    Update twoup.js
    First of the Codacy fixes

--- commands/Economy/twoup.js
+++ commands/Economy/twoup.js
@@ -10,38 +10,23 @@ exports.run = async (client, message, [credit]) => {
         else {
             var z; let rolls = [];
             for (z = 0; z < 7; z++) {
-                y = Math.random();
-                rolls[z] = (y > .5) ? "heads" : "tails";
+                var x = (Math.random() > .5) ? "heads" : "tails";
+                rolls.push(x);
             }
 
-            function winning() {
+            var earnings; var result;
+
+            if (((rolls[0] === rolls[1]) && (rolls[0] === "heads")) || 
+                ((rolls[1] === rolls[2]) && (rolls[1] === "heads")) ||
+                ((rolls[2] === rolls[3]) && (rolls[2] === "heads")) ||
+                ((rolls[3] === rolls[4]) && (rolls[3] === "heads")) ||
+                ((rolls[4] === rolls[5]) && (rolls[4] === "heads")) ||
+                ((rolls[5] === rolls[6]) && (rolls[5] === "heads")) ||
+                ((rolls[6] === rolls[7]) && (rolls[6] === "heads"))) {  
                 earnings = (credit * 1.25).toFixed(0);
                 credit = parseInt(row.credits) + parseInt(earnings);
                 db.run(`UPDATE scores SET credits = ${credit} WHERE userId = ${message.author.id}`);
-                return text = `Coins ${rolls} You have won ${earnings} credits.`;
-            }
-
-            if ((rolls[0] === rolls[1]) && (rolls[0] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[1] === rolls[2]) && (rolls[1] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[2] === rolls[3]) && (rolls[2] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[3] === rolls[4]) && (rolls[3] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[4] === rolls[5]) && (rolls[4] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[5] === rolls[6]) && (rolls[5] === "heads")) {
-                winning();
-                return message.reply(text);
-            } if ((rolls[6] === rolls[7]) && (rolls[6] === "heads")) {
-                winning();
-                return message.reply(text);
+                return message.reply(`Coins ${rolls} You have won ${earnings} credits.`);
             }
             if ((rolls[0] !== rolls[1]) && (rolls[0] === rolls[2]) && (rolls[0] !== rolls[3]) && (rolls[0] === rolls[4]) && (rolls[0] !== rolls[5]) && (rolls[0] === rolls[6]) && (rolls[0] !== rolls[7])) {
                 earnings = (credit * 2.25).toFixed(0);
@@ -74,5 +59,5 @@ exports.help = {
     description: "Bet your credits on coin flips. Get two heads in a row and win or hope for all five odds!",
     usage: "[credits:int]",
     usageDelim: " ",
-    extendedHelp: 'Two-up is a traditional Australian gambling game, involving a designated "spinner" throwing two coins into the air. Players bet on whether the coins will fall with both heads up, both tails up, or with one head and one tail up (known as "odds"). It is traditionally played on Anzac Day in pubs and clubs throughout Australia, in part to mark a shared experience with Diggers through the ages.',
+    extendedHelp: "Two-up is a traditional Australian gambling game, involving a designated 'spinner' throwing two coins into the air. Players bet on whether the coins will fall with both heads up, both tails up, or with one head and one tail up (known as 'odds'). It is traditionally played on Anzac Day in pubs and clubs throughout Australia, in part to mark a shared experience with Diggers through the ages.",
 };
