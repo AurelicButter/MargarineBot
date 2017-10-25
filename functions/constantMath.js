494fd7252522c658a633ea279089d3aecf8833f1
Butterstroke <konirosen@protonmail.ch>
2017-10-24 21:16:50 -0400

    Update constantMath.js

--- functions/constantMath.js
+++ functions/constantMath.js
@@ -1,11 +1,11 @@
 module.exports = (client, message, number) => {
-  if (Number(number) == number) { number = number; }
-  else if (number == "e") { number = Math.E; }
-  else if (number == "pi") { number = Math.PI; }
+  if (Number(number) === number) { number = number; }
+  else if (number === "e") { number = Math.E; }
+  else if (number === "pi") { number = Math.PI; }
   else if (number.includes("sqrt")) { number = Math.sqrt(number.slice(4)); }
   else if (number.includes("abs")) { number = Math.abs(number.slice(3)); }
   else if (number.includes("ln")) { number = Math.LN(number.slice(2)); }
-  else if (number.includes("log")) { number == Math.LOG(number.slice(3))}
+  else if (number.includes("log")) { number = Math.LOG(number.slice(3))}
   else { number = null; }
   return Number(number);
 };
