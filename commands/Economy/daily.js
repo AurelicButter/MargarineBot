a9260fda961b66ff354007fff710b02a32744bea
Butterstroke <konirosen@protonmail.ch>
2017-10-28 18:37:18 -0400

    Update daily.js
    Fix for user search

--- commands/Economy/daily.js
+++ commands/Economy/daily.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, [member]) => {
 
     var user = client.funcs.userSearch(client, message, member);
     
-    if (user.username === null) { return; }
+    if (user.username === undefined) { return; }
     if (user.bot === true) { return message.reply("You can't give your credits to a bot user!"); }
 
     db.get(`SELECT * FROM scores WHERE userId = "${user.id}"`, [], (err, row) => {
