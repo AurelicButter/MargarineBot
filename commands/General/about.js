e067ba8a2b14026810933cafe3277186fbeff39b
Butterstroke <konirosen@protonmail.ch>
2017-11-11 13:39:36 -0500

    Update about.js

--- commands/General/about.js
+++ commands/General/about.js
@@ -4,10 +4,10 @@ exports.run = async (client, message) => {
   const embed = new client.methods.Embed()
      .setColor("#37FDFC")
      .setTimestamp()
-     .setTitle("About Margarine")
+     .setTitle(`About ${client.user.username}`)
      .setDescription(`I am a lovely and helpful bot (*Sometimes.*). Doing ${config.prefix}help is great for finding ways I can assist you. I was written in Komada, a Discord.js framework.\n
      \n**Name Origin:** Butterstroke#7150's typical nickname is Butter. As in the stuff that you put on toast. My name comes from the artificial butter *(Butterstroke#7150 tends to call it 'Fake Butter')* you can buy in stores called, Margarine.\n
-     \n**Creation:** I was created on ${client.user.createdAt.toLocaleString()} by ${client.users.find("id", `${config.ownerID}`).tag}. My current version is ${config.version} as of ${config.updateDate}.\n
+     \n**Creation:** I was created on ${client.user.createdAt.toLocaleString()} by ${client.owner.tag}. My current version is ${config.version} as of ${config.updateDate}.\n
      \n**More Information:** I have a Github repo which contains my current update tracker and the source code if you are curious. https://github.com/Butterstroke/MargarineBot`)
      .setThumbnail(client.user.avatarURL());
   return message.channel.send({embed});
@@ -24,6 +24,6 @@ exports.conf = {
    
  exports.help = {
      name: "about",
-     description: "General information about Margarine.",
+     description: "General information about the bot.",
      usage: "",
  };
