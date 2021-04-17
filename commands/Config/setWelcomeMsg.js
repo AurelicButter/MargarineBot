87c9a06beb00a8cccf5b8f2ebad0af0337d26838
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-04-17 17:05:55 -0300

    Allow Moderators to See Currently Set Messages
    Allow server moderators to see the currently set message for welcoming and leaving users.
    
    Downgraded permission level for setWelcome and setLeave to allow server moderators to edit the messages

--- commands/Config/setWelcomeMsg.js
+++ commands/Config/setWelcomeMsg.js
@@ -6,15 +6,26 @@ module.exports = class extends Command {
 			name: "setwelcome",
 			enabled: true,
 			runIn: ["text"],
-			description: "Set a welcome message for new guild members to see when they join!",
-			usage: "[remove|text:str]",
-			permissionLevel: 6,
+			description: "Set a welcome message for new users to see when they join!",
+			usage: "[show|remove|text:str]",
+			permissionLevel: 5,
 			extendedHelp: "Add `-pinguser`, `-username`, `-userid`, and/or `-servername` to the message to have Margarine personalize it! Standard Discord styling still applies.\n\n" +
                 "This feature requires both a message and a default channel set. Removing either one will disable this feature."
 		});
+
+		this.humanUse = "[show|remove|(welcome message)]";
 	}
 
 	async run(msg, [item]) {
+		if (item === "show" || item === undefined) {
+			return msg.channel.send(
+				msg.guild.settings.welcomeMsg.replace("-pinguser", `<@!${msg.author.id}>`)
+					.replace("-servername", msg.guild.name)
+					.replace("-userid", msg.author.id)
+					.replace("-username", msg.author.username)
+			);
+		}
+
 		if (item === "remove") {
             msg.guild.settings.update("welcomeMsg", null).then(() => {
                 msg.sendLocale("REMOVEWELCOME", [msg]);
