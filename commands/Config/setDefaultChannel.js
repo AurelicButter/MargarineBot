1a35c0d7c44fe71ef153f49474f8a6a76538cefc
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-04-27 21:06:42 -0300

    Cleanup Before Release

--- commands/Config/setDefaultChannel.js
+++ commands/Config/setDefaultChannel.js
@@ -15,8 +15,8 @@ module.exports = class extends Command {
     }
 
     async run(msg, [item]) {
-        if (item && !item.includes("<#")) { return msg.sendLocale("RESOLVER_INVALID_CHANNEL", [item]); }
         if (item === "remove") { item = null; }
+        if (item && !item.includes("<#")) { return msg.sendLocale("RESOLVER_INVALID_CHANNEL", [item]); }
 
         msg.guild.settings.update("defaultChannel", item).then(() => {
             if (item) { return msg.sendLocale("SETDEFAULTCHANNEL", [msg, item]); }
