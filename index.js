0c3e3ad9cf982de0576e71e8dfc8eeaab0cb93a8
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-07-29 18:36:55 -0300

    OwnerSetting Collection Popped Up Again

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ const { existsSync } = require("fs");
 envCheck(); //Checks to make sure Margarine is running in the right enviroment.
 
 const client = new Client({
-    fetchAllMembers: false,
-    prefix: config.prefix,
-    language: "en-CA",
-    commandEditing: true,
-    readyMessage: (client) => `This is ${client.user.username} speaking! Online and awaiting orders!\nI'm currently serving ${client.guilds.cache.size} guilds and ${client.users.cache.size} people!`
+	fetchAllMembers: false,
+	prefix: config.prefix,
+	language: "en-CA",
+	commandEditing: true,
+	readyMessage: (client) =>
+		`This is ${client.user.username} speaking! Online and awaiting orders!\nI'm currently serving ${client.guilds.cache.size} guilds and ${client.users.cache.size} people!`
 });
 
 Client.defaultPermissionLevels
-    .add(5, ({ guild, member }) => guild && member.roles.cache.has(guild.settings.modRole))
-    .add(6, ({ guild, member }) => guild && member.permissions.has("ADMINISTRATOR"))
-    .add(7, ({ guild, member }) => guild && guild.ownerID === member.id)
-    .add(9, ({ author, client }) => author === client.owner || client.secondary.includes(author.id))
-    .add(10, ({ author, client }) => author === client.owner);
+	.add(5, ({ guild, member }) => guild && member.roles.cache.has(guild.settings.modRole))
+	.add(6, ({ guild, member }) => guild && member.permissions.has("ADMINISTRATOR"))
+	.add(7, ({ guild, member }) => guild && guild.ownerID === member.id)
+	.add(9, ({ author, client }) => author === client.owner || client.secondary.includes(author.id))
+	.add(10, ({ author, client }) => author === client.owner);
 
 schemaManager(client); //Adds all configurable settings.
 
@@ -27,9 +28,10 @@ client.speech = speech;
 client.dataManager = dataManager;
 client.util = util; //All utility functions and extra search functions
 
-if (!existsSync(config.database)) { dataManager("init"); } //Init the SQLite Database
+if (!existsSync(config.database)) {
+	dataManager("init");
+} //Init the SQLite Database
 
-client.ownerSetting = new Collection();
 client.music = new Collection();
 
 client.build = config.build;
